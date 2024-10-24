36d328df63cfedcc65ccf44bec32a1e972178891
mesaprotector <162765448+mesaprotector@users.noreply.github.com>
2024-10-24 16:31:43 -0700

    Faster timeout to avoid accidentally typing in during delay

--- waitcomment.sh
+++ waitcomment.sh
@@ -15,7 +15,7 @@ curr_shell="$(head -n1 /tmp/comment.tmp | cut -c 31-36)"
 if grep -qE '=/usr/bin/visudo|=/usr/bin/vim|=/usr/bin/rm|=/usr/bin/rmdir|=/usr/bin/mkdir|=/usr/bin/trash-put|=/usr/bin/cp|=/usr/bin/touch|=/usr/bin/mv|=/usr/bin/chmod|=/usr/bin/chown|=/usr/bin/pacman -S |=/usr/bin/pacman -R|>' <<< $curr_command; then
 	# Keeps sudo-comment from triggering itself in certain situations.
 	if grep -qE -v 'comment.tmp|changed.tmp' <<< $curr_command; then
-		sleep 0.5
+		sleep 0.2
 		# Waits until no processes running in foreground on given shell except bash or sudo. Causes some problems with files named bash (ex. /etc/bash.bashrc). Should use something
 		# other than grep but I'll figure it out later.
 		until [ `ps ax | grep -v "bash" | grep -v "sudo -s" | grep -v "sudo -i" | awk '$3 ~ /\+/' | awk -v shell=$curr_shell '$2 ~ shell' | wc -l` == 0 ]
